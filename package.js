b8ba692983b61a065f244c2a7299b9154eb6fb17
Scott Beck <scottbeck@gmail.com>
2013-11-02 12:50:01 +0400

    requires underscore

--- package.js
+++ package.js
@@ -2,8 +2,8 @@ Package.describe({
   summary: "underscore.inflection repackaged for Meteor"
 });
 
-Package.on_use(function (api, where) {
-  where = where || ['client', 'server'];
-
+Package.on_use(function (api) {
+  where = ['client', 'server'];
+  api.use('underscore', where);
   api.add_files('lib/underscore.inflection.js', where);
 });
